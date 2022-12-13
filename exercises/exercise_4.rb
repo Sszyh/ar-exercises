8c947e1df9cf9b3bfb0c30cb5dd354a9d5ea8ccb
Sszyh <zhuyer@outlook.com>
2022-12-12 23:29:25 -0800

    do fetch amd print output

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -11,7 +11,12 @@ Surrey = Store.create(name: "Surrey", annual_revenue:224000, mens_apparel:false,
 Whistler = Store.create(name: "Whistler", annual_revenue:1900000, mens_apparel:true, womens_apparel:false)
 Yaletown = Store.create(name: "Yaletown", annual_revenue:430000, mens_apparel:true, womens_apparel:true)
 
-@mens_stores = Store.where(mens_apparel: true)
+@mens_stores = Store.where(mens_apparel: true, womens_apparel:false)
 @mens_stores.each do |m|
+  puts "Name: #{m.name}, Annual Revenew #{m.annual_revenue}"
+end
+
+@womens_stores = Store.where("annual_revenue < :num", {num: 1000000}, womens_apparel:true)
+@womens_stores.each do |m|
   puts "Name: #{m.name}, Annual Revenew #{m.annual_revenue}"
 end
