983b37f82e66457296cdf1b73105c34afe940f60
Sszyh <zhuyer@outlook.com>
2022-12-13 13:50:50 -0800

    do calculations using 'where' and 'count'

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum("annual_revenue")
+@average_annual_revenue = @total_revenue / Store.count
+@number_of_store = Store.where("annual_revenue >= :num",{num: 1000000}).count
+puts @total_revenue
+puts @average_annual_revenue
+puts @number_of_store
